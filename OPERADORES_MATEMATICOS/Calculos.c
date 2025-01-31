622214f8d6ee182459e00dbe203fd84bf528880d
LucasMartinsmg <lucasamptrader@gmail.com>
2025-01-31 15:51:10 +0000

    Calculos1

---
+++ OPERADORES_MATEMATICOS/Calculos.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+/* PRINCIPAIS OPERADORES MATEMATICOS
+- SOMA (+)
+- SUBTRAÇÃO (-)
+- MULTIPLICAÇÃO (*)
+- DIVISÃO (/)                                            */
+int numero1, numero2;
+int soma, subtracao, multiplicacao, divisao;
+
+printf("Insira numero1:\n");
+scanf("%d", &numero1);
+printf("Insira numero2:\n");
+scanf("%d", &numero2);
+
+soma = numero1 + numero2;
+subtracao = numero1 - numero2;
+multiplicacao = numero1 * numero2;
+divisao = numero1 / numero2;
+
+printf("A soma e: %d\n", soma);
+
+
+
+
+
+}
